c99e8578a37492d8ac8f82772d3b0235c4919edf
givionte <dandmcd@live.com>
2019-01-10 13:06:39 +0800

    Websocket build fix

---
+++ client/build/precache-manifest.4b1b33116a1f2df078ee4b39dc83cbce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4670136fda19443ab808",
+    "url": "/static/js/main.4670136f.chunk.js"
+  },
+  {
+    "revision": "92a32e8918d2a209c4b2",
+    "url": "/static/js/1.92a32e89.chunk.js"
+  },
+  {
+    "revision": "cff9661de1e5411e9c6925e9e75e931f",
+    "url": "/index.html"
+  }
+];
