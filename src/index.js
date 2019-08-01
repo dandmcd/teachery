b7aded0d4154b17030d90aa4c7ab4e7f06a5ebe7
givionte <dandmcd@live.com>
2019-08-01 18:57:50 +0800

    Deployment

--- src/index.js
+++ src/index.js
@@ -103,6 +103,6 @@ sequelize.sync({ force: isTest || isProduction }).then(async () => {
 
 app.use(express.static("build"));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
