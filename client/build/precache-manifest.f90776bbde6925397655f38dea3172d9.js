b90471b22077e1ac4cafbeddb8f26848fe5ae420
givionte <dandmcd@live.com>
2019-08-02 14:32:36 +0800

    Deployment

---
+++ client/build/precache-manifest.f90776bbde6925397655f38dea3172d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ed5831497bcef92be0826f8bfd17d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9016700eb2278dec1c01",
+    "url": "/static/css/main.1a152bf3.chunk.css"
+  },
+  {
+    "revision": "f6890881a91c18eb221d",
+    "url": "/static/js/2.be9e269a.chunk.js"
+  },
+  {
+    "revision": "9016700eb2278dec1c01",
+    "url": "/static/js/main.2e61d5f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bca34be56460bbc9ae5e87c0f646e97",
+    "url": "/static/media/student.7bca34be.jpg"
+  },
+  {
+    "revision": "37f9f5a9763411ecdab4fb0525d74611",
+    "url": "/static/media/teacher.37f9f5a9.jpg"
+  }
+]);
