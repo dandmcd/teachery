9d79b21718e57d156ddeea28e8fa5eccae8fdfa7
givionte <dandmcd@live.com>
2019-08-04 15:38:28 +0800

    Deployment

--- src/index.js
+++ src/index.js
@@ -99,8 +99,14 @@ sequelize.sync({ force: isTest || isProduction }).then(async () => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
