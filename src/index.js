3ca2af261320309a551f3bf77fe9d1c618ba6a4f
givionte <dandmcd@live.com>
2019-08-10 23:34:54 +0800

    Heroku routing change

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ sequelize.sync({ force: isTest }).then(async () => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 } else {
   app.use(express.static(path.join(__dirname, "/client/public")));
