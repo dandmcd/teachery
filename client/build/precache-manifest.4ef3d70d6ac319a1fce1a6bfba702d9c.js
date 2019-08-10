d739a1c32391247eb90c1ed7188c1cb5b6a6c9fa
givionte <dandmcd@live.com>
2019-08-10 22:56:41 +0800

    New Heroku build

---
+++ client/build/precache-manifest.4ef3d70d6ac319a1fce1a6bfba702d9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29dec581ce6aaa2dc5ebb6d22f4992c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0d24e33340314ff77c4",
+    "url": "/static/css/main.25ada5a7.chunk.css"
+  },
+  {
+    "revision": "290ae8f6cef38dd9976d",
+    "url": "/static/js/2.ca1058c3.chunk.js"
+  },
+  {
+    "revision": "a0d24e33340314ff77c4",
+    "url": "/static/js/main.b651e43e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
